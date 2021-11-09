eb1b85c71e1b6c5c295eda20e34ec0d66b4ac0b1
ArtyomBru <93974671+ArtyomBru@users.noreply.github.com>
2021-11-09 12:15:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {    //3.3
+            try
+            {
+                Console.WriteLine("Введите n:");
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите x:");
+                double x = double.Parse(Console.ReadLine());
+                double s = 0;
+                for (int i = 1, h = 1; i < n; i++, h += 2)
+                {
+                    s += (1 + 2 / 1! * x / 2 + 5 / 2! * Math.Pow(x, 2) / 2 + 10 / 3! * Math.Pow(x, 3) / 2 * n);
+                }
+                Console.WriteLine("s={0:F2}",s);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
