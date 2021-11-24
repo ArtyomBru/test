378b77bf043061d77b14bbc179191e69012ac1e6
ArtyomBru <93974671+ArtyomBru@users.noreply.github.com>
2021-11-24 18:43:48 +0300

    Add files via upload

---
+++ 3.1 базовый 20 вар.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //3.1. Базовый уровень 20 вариант
+            try
+            {
+                Console.WriteLine("Введите N:");
+                int N = int.Parse(Console.ReadLine());
+                for (int i = 10; i < N; i++)
+                {
+                    int m = i;
+                    int min = int.MaxValue;
+                    while (m != 0)
+                    {
+                        if (m! / 2 < min) min = m! / 10;
+                        m /= 5;
+                    }
+                    Console.WriteLine(i + " " + min);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
