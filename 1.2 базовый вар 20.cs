c3468e6e55748a14c08adebe1c6d7ae80e4b3f81
ArtyomBru <93974671+ArtyomBru@users.noreply.github.com>
2021-11-26 15:49:57 +0300

    Add files via upload

---
+++ 1.2 базовый вар 20.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите L:");
+            int L = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите I:");
+            int I = int.Parse(Console.ReadLine());
+            int W = L * (I * I) / 2;
+            Console.WriteLine(W);
+        }
+    }
+}
